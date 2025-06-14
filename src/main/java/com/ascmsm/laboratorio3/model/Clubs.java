90fea3db503cdb1b0c42f5dc85e1c8992c4e7fd9
Marroquin02 <carlosmarroquin600000@gmail.com>
2025-06-14 07:31:08 -0600

    Add Clubs entity and repository interface

--- src/main/java/com/ascmsm/laboratorio3/model/Clubs.java
+++ src/main/java/com/ascmsm/laboratorio3/model/Clubs.java
@@ -1,4 +1,20 @@
 package com.ascmsm.laboratorio3.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Clubs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String country;
+    private String coach;
+    private int titles;
 }
--- src/main/java/com/ascmsm/laboratorio3/repository/ClubsRepository.java
+++ src/main/java/com/ascmsm/laboratorio3/repository/ClubsRepository.java
@@ -1,4 +1,8 @@
 package com.ascmsm.laboratorio3.repository;
 
-public class ClubsRepository {
+import com.ascmsm.laboratorio3.model.Clubs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubsRepository extends JpaRepository<Clubs, Long> {
+
 }
